408bf2ba2caa5f4fa77e5e7f7f04db92973284b7
JesperMyhre <jesper.ovreness@gmail.com>
2024-09-23 17:48:08 +0200

    lagt til svg kart

--- Oblig1/scripts/index.js
+++ Oblig1/scripts/index.js
@@ -1,12 +1,12 @@
-document.querySelectorAll('.zoom-btn').forEach(button => {
-  button.addEventListener('click', function() {
+document.querySelectorAll('.land').forEach(path => {
+  path.addEventListener('click', function() {
     const map = document.getElementById('map');
-    const rect = button.getBoundingClientRect();
+    const rect = path.getBoundingClientRect();
     const mapRect = map.getBoundingClientRect();
     
-    const zoomLevel = 2; // Hvor mye som zoomes inn
-    const offsetX = (rect.left + rect.width / 2) - (mapRect.left + mapRect.width / 2);
-    const offsetY = (rect.top + rect.height / 2) - (mapRect.top + mapRect.height / 2.7);
+    const zoomLevel = 5; // Hvor mye som zoomes inn
+    const offsetX = (rect.left + rect.width / 1) - (mapRect.left + mapRect.width / 2.4);
+    const offsetY = (rect.top + rect.height / 5) - (mapRect.top + mapRect.height / 2.35);
     
     //Kan sette spesifikke koordinater på translate
     map.style.transform = `scale(${zoomLevel}) translate(${-offsetX}px, ${-offsetY}px)`;
