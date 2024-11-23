9bb5d5f2a335c114a2eb6941ca25f340b2a03be8
JesperMyhre <jesper.ovreness@gmail.com>
2024-11-23 17:48:50 +0100

    justeringer artikkel 2

--- Oblig1/scripts/nav.js
+++ Oblig1/scripts/nav.js
@@ -1,13 +1,12 @@
 const navElement = document.querySelector("nav");
 
 window.addEventListener("scroll", () => {
+  console.log("Scroll event detected"); // Debugging log
   if (window.scrollY > 50) {
+    console.log("Adding navScroll class"); // Debugging log
     navElement.classList.add("navScroll");
   } else {
+    console.log("Removing navScroll class"); // Debugging log
     navElement.classList.remove("navScroll");
   }
-});
-
-window.addEventListener("scroll", () => {
-  console.log(window.scrollY + "yes we are scrolling");
 });
