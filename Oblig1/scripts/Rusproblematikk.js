23af6081701cb41acca0ba4f8d3a5811b183828c
ErikAJensen <111729359+ErikAJensen@users.noreply.github.com>
2024-11-25 11:12:00 +0100

    telefon

--- Oblig1/scripts/Rusproblematikk.js
+++ Oblig1/scripts/Rusproblematikk.js
@@ -2,6 +2,40 @@ document.addEventListener("mousemove", function (e) {
   createSmoke(e.pageX, e.pageY);
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+  const ctx = document.getElementById("rusbrukChart").getContext("2d");
+  const rusbrukChart = new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: ["Total Rusbrukslidelse", "Alkohollidelser", "Stofflidelser"],
+      datasets: [
+        {
+          label: "Prosentandel av studenter",
+          data: [12.3, 8.8, 4.4],
+          backgroundColor: [
+            "rgba(75, 192, 192, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(255, 99, 132, 0.2)",
+          ],
+          borderColor: [
+            "rgba(75, 192, 192, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 99, 132, 1)",
+          ],
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  });
+});
+
 function createSmoke(x, y) {
   const smoke = document.createElement("div");
   smoke.classList.add("smoke-particle");
